42b17c9b6ea32a5c3da7adcec1b9e07eb6a6253a
Akshay <akshaythakur26t@gmail.com>
2025-05-21 14:54:20 +0530

    All changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://thakurakshay:thakurakshay321@cluster0.3h9ld.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
    
 }
